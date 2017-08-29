159192f6f3de34ec52b163d565682c5e90f3bdfe
RENT <wartalski.jan@gmail.com>
2017-08-29 18:15:27 +0200

    comiit1

--- app/src/main/java/io/github/wzieba/listofapps/AppAdapter.java
+++ app/src/main/java/io/github/wzieba/listofapps/AppAdapter.java
@@ -14,7 +14,7 @@ import lombok.Data;
 
 @Data
 public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder> {
-
+//fthfgh
     private final List<AppInfo> appInfos;
 
     @Override
